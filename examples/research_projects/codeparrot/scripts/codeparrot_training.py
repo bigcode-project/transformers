9af45a80c780c2082d0de50b24274f278b808a3c
Niklas Muennighoff <n.muennighoff@gmail.com>
2022-12-09 15:04:49 +0400

    Update examples/research_projects/codeparrot/scripts/codeparrot_training.py
    Co-authored-by: Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>

--- examples/research_projects/codeparrot/scripts/codeparrot_training.py
+++ examples/research_projects/codeparrot/scripts/codeparrot_training.py
@@ -121,7 +121,7 @@ def setup_logging(args):
 
 def create_dataloaders(args):
     ds_kwargs = {"streaming": args.no_streaming}
-    train_data = load_dataset(args.dataset_name_train, split="train", **ds_kwargs)
+    train_data = load_dataset(args.dataset_name_train, split="train", use_auth_token=True)
     train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
     if args.dataset_name_train == args.dataset_name_valid:
         # Split the dataset into train and validation
