92d98c176efe5c6dee96c3c55197025e84860e22
Joel Lamy-Poirier <joel.lamy-poirier@servicenow.com>
2023-02-14 15:20:29 -0500

    Megatron conversion script (#8)

--- src/transformers/models/gpt_bigcode/configuration_gpt_bigcode.py
+++ src/transformers/models/gpt_bigcode/configuration_gpt_bigcode.py
@@ -19,10 +19,9 @@ from enum import Enum
 from typing import Any, List, Mapping, Optional
 
 from transformers import PreTrainedTokenizer, TensorType, is_torch_available
-
-from ...configuration_utils import PretrainedConfig
-from ...onnx import OnnxConfigWithPast, PatchingSpec
-from ...utils import logging
+from transformers.configuration_utils import PretrainedConfig
+from transformers.onnx import OnnxConfigWithPast, PatchingSpec
+from transformers.utils import logging
 
 
 logger = logging.get_logger(__name__)
