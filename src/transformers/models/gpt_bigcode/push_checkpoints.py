5cfb845db57ea91732494114fd191b5506557af3
RaymondLi0 <raymond.li@servicenow.com>
2023-11-14 05:40:35 +0000

    add option to re-push past iterations

--- src/transformers/models/gpt_bigcode/push_checkpoints.py
+++ src/transformers/models/gpt_bigcode/push_checkpoints.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import re
 import subprocess
@@ -38,12 +39,38 @@ def main(argv=None):
         default=1,
         help="Iteration number must be divisble by iter_interval in order to be pushed",
     )
+    parser.add_argument(
+        "--iters",
+        type=int,
+        nargs='+',
+        default=None,
+        help="Specify a list of iterations to push. If None (default), will potentially push all the checkpoints (subject to iter_interval)",
+    )
+    parser.add_argument(
+        "--tokenizer",
+        type=str,
+        default=None,
+        help="Path to tokenizer file to commit before the checkoints.",
+    )
+    parser.add_argument(
+        "--push_past_iters",
+        action="store_true",
+        default=False,
+        help="If True, also push iterations that are lower than the last commit.",
+    )
     args, argv = parser.parse_known_args(argv)
 
     save_dir = args.save_dir or args.exp_dir / "hf_checkpoints"
 
     hf_repo = Repository(save_dir, clone_from=args.repo_name)
     hf_repo.git_checkout(args.branch_name, create_branch_ok=True)
+    
+    # Pull latest changes
+    env = os.environ.copy()
+    env["GIT_LFS_SKIP_SMUDGE"] = "1"
+    git_pull_output = subprocess.run(["git", "pull"], cwd=save_dir, capture_output=True, env=env)
+    print(git_pull_output)
+    
     # Find last checkpoint that was uploaded
     head_hash = hf_repo.git_head_hash()
     commit_msg = subprocess.check_output(["git", "show", "-s", "--format=%B", head_hash], cwd=save_dir).decode()
@@ -54,15 +81,19 @@ def main(argv=None):
         last_commit_iter = -1
 
     # The checkpoint dirs should be in ascending iteration order, so that the last commit corresponds to the latest checkpoint
-    ckpt_dirs = sorted(
-        [x for x in (args.exp_dir / "checkpoints").iterdir() if re.match(r"(\d+)", x.name) and x.is_dir()],
-        key=lambda p: get_iter_number(p.name)
-    )
+    ckpt_dirs = [x for x in (args.exp_dir / "checkpoints").iterdir() if re.match(r"(\d+)", x.name) and x.is_dir()]
+    if args.iters is not None:
+        args.iters = [int(n) for n in args.iters]
+        ckpt_dirs = [p for p in ckpt_dirs if get_iter_number(p.name) in args.iters]
+    ckpt_dirs = sorted(ckpt_dirs, key=lambda p: get_iter_number(p.name))
     print(f"Found the following checkpoints: {ckpt_dirs}")
+    
+    if args.tokenizer is not None:
+        raise NotImplementedError("Push tokenizer not implemented yet")
 
     for ckpt_dir in ckpt_dirs:
         iter_number = get_iter_number(ckpt_dir.name)
-        if iter_number <= last_commit_iter:
+        if not args.push_past_iters and iter_number <= last_commit_iter:
             continue
         if iter_number % args.iter_interval == 0:
             print(f"Converting iteration {iter_number}")
